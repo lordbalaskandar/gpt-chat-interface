8bc271c62f84665df768744c86885a3d2c6256c9
Thanushen Balaskandar <thanushenbalaskandar@gmail.com>
2025-05-27 18:30:29 +0100

    env variables

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env['URL'];
-const API_KEY = process.env['API_KEY']
+const baseURL = process.env['NEXT_PUBLIC_API_URL'];
+const API_KEY = process.env['NEXT_PUBLIC_API_KEY']
 
 const api = axios.create({
   baseURL,
